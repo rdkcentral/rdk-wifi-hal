751dc83b55ff07fa8a8e639586642ae3561f144b
sjs889 <Srijeyarankesh_JS@comcast.com>
2025-06-07 16:44:39 +0000

    Adding password to hostapd structure

--- src/wifi_hal_hostapd.c
+++ src/wifi_hal_hostapd.c
@@ -745,7 +745,22 @@ int update_security_config(wifi_vap_security_t *sec, struct hostapd_bss_config *
     conf->rdkb_eap_request_retries = sec->eap_req_retries;
 #endif
     if (conf->ieee802_1x || is_open_sec_radius_auth(sec) || conf->mdu) {
-        wifi_radius_settings_t *radius_cfg = conf->mdu ? &sec->repurposed_radius : &sec->u.radius;
+        wifi_radius_settings_t *radius_cfg = NULL;
+        if (conf->mdu) {
+            radius_cfg = &sec->repurposed_radius;
+            snprintf(conf->ssid.wpa_passphrase, sizeof(conf->ssid.wpa_passphrase), "%s", sec->u.key.key);
+            conf->ssid.wpa_passphrase_set = true;
+            wifi_util_info_print("%s:%d:SREESH wpa_passphrase:%s\n", __func__, __LINE__, conf->ssid.wpa_passphrase);
+            conf->osen = 0;
+        } else {
+            radius_cfg = &sec->u.radius;
+        }
+
+        if (!radius_cfg) {
+            wifi_util_error_print("%s:%d:Invalid radius server configuration in VAP setting\n", __func__, __LINE__);
+            return RETURN_ERR;
+        }
+
         conf->disable_pmksa_caching = sec->disable_pmksa_caching;
         if (radius_cfg->ip == 0) {
             wifi_hal_error_print("%s:%d:Invalid radius server IP configuration in VAP setting\n", __func__, __LINE__);
