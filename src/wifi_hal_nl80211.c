13a7fad1e2ded1abae4352d7aebbb6dcde64054e
Pavithra Sundaravadivel <Pavithra_Sundaravadivel@Comcast.com>
2025-08-25 10:19:32 +0000

    RDKB-36610 check crash
    Impacted Platforms:
    All RDKB Platforms
    
    Reason for change:
    
    Test Procedure:
    
    Risks:
    
    Priority:
    
    Signed-off-by:Pavithra_Sundaravadivel@comcast.com

--- src/wifi_hal_nl80211.c
+++ src/wifi_hal_nl80211.c
@@ -8750,7 +8750,7 @@ int nl80211_connect_sta(wifi_interface_info_t *interface)
     memset(interface->wpa_s.current_bss, 0, sizeof(struct wpa_bss) + bss_ie->buff_len);
     strcpy(interface->wpa_s.current_bss->ssid, backhaul->ssid);
     interface->wpa_s.current_bss->ssid_len = strlen(backhaul->ssid);
-    wifi_hal_dbg_print("%s:%d: ssid is %s and len is %u\n", __func__, __LINE__, interface->wpa_s.current_bss->ssid, interface->wpa_s.current_bss->ssid_len);
+    wifi_hal_dbg_print("%s:%d: ssid is %s and len is %lu\n", __func__, __LINE__, interface->wpa_s.current_bss->ssid, interface->wpa_s.current_bss->ssid_len);
     memcpy(interface->wpa_s.current_bss->bssid, backhaul->bssid, ETH_ALEN);
     memcpy(interface->wpa_s.current_ssid->bssid, backhaul->bssid, ETH_ALEN);
     if (security->encr == wifi_encryption_aes) {
@@ -8898,7 +8898,7 @@ int nl80211_connect_sta(wifi_interface_info_t *interface)
         radio->oper_param.band == WIFI_FREQUENCY_6_BAND) {
         interface->wpa_s.conf->sae_pwe = 1;
 
-	wifi_hal_error_print("%s:%d: ssid %s len %u sae passsword %s len %u\n", __func__, __LINE__, interface->wpa_s.current_ssid->ssid, interface->wpa_s.current_ssid->ssid_len, interface->wpa_s.current_ssid->sae_password, os_strlen(interface->wpa_s.current_ssid->sae_password));
+	wifi_hal_error_print("%s:%d: ssid %s len %lu sae passsword %s len %lu\n", __func__, __LINE__, interface->wpa_s.current_ssid->ssid, interface->wpa_s.current_ssid->ssid_len, interface->wpa_s.current_ssid->sae_password, os_strlen(interface->wpa_s.current_ssid->sae_password));
         interface->wpa_s.current_ssid->pt = sae_derive_pt(interface->wpa_s.conf->sae_groups,
             interface->wpa_s.current_ssid->ssid,
             interface->wpa_s.current_ssid->ssid_len,
