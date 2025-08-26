d8ce646b48e2ef5661dcc5f10c2e5a469ac0275c
Sanjay Venkatesan <Sanjay_Venkatesan@Comcast.com>
2025-08-26 07:38:27 +0000

    updated comments

--- src/wifi_hal_nl80211.c
+++ src/wifi_hal_nl80211.c
@@ -8369,7 +8369,10 @@ static int wifi_hal_emu_set_assoc_clients_stats_data(unsigned int vap_index, boo
             nlmsg_free(msg);
             return -1;
         }
-
+        //print the MLD MAC address from the stats structure
+        wifi_hal_stats_dbg_print("SJY %s:%d: MLD MAC address is %02x:%02x:%02x:%02x:%02x:%02x\n", __func__, __LINE__,
+            stats[i].cli_MLDAddr[0], stats[i].cli_MLDAddr[1], stats[i].cli_MLDAddr[2],
+            stats[i].cli_MLDAddr[3], stats[i].cli_MLDAddr[4], stats[i].cli_MLDAddr[5]);
         nlattr_sta_info = nla_nest_start(msg, RDK_VENDOR_ATTR_STA_INFO);
         if (!nlattr_sta_info) {
             wifi_hal_stats_error_print("%s:%d: Failed to add station list attribute for vap index %d\n", __func__, __LINE__, vap_index);
@@ -8402,7 +8405,7 @@ static int wifi_hal_emu_set_assoc_clients_stats_data(unsigned int vap_index, boo
             nla_put_u32(msg, RDK_VENDOR_ATTR_STA_INFO_ASSOC_NUM, stats[i].cli_Associations) < 0 ||
             nla_put_u64(msg, RDK_VENDOR_ATTR_STA_INFO_TX_RETRIES, cli_RetryCount) < 0 ||
             nla_put_u64(msg, RDK_VENDOR_ATTR_STA_INFO_RX_ERRORS, stats[i].cli_RxErrors) < 0 ||
-            nla_put(msg, RDK_VENDOR_ATTR_STA_INFO_MLD_MAC, ETH_ALEN, mld_zero_mac) < 0 ) {
+            nla_put(msg, RDK_VENDOR_ATTR_STA_INFO_MLD_MAC, ETH_ALEN, stats[i].cli_MLDAddr) < 0 ) {
 
             nla_nest_cancel(msg, nlattr_sta_info);
             nlmsg_free(msg);
