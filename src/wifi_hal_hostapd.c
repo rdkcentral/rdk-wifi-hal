da92b408bcc43b73056a6065f531f8bae71a6b74
navyasher <navyasheregar@comcast.com>
2025-10-30 15:41:04 +0530

    RDKB-0 Fixing coverity issues Reason for change: CID 277488, 277385, 338904, 338903
    Test Procedure:
    1. Build image and flash in tcxb8 device.
    2. Onewifi should be running.
    
    Risks: None
    Signed-off-by: Navya_Sheregar@comcast.com

--- src/wifi_hal_hostapd.c
+++ src/wifi_hal_hostapd.c
@@ -1473,7 +1473,9 @@ int update_hostap_iface(wifi_interface_info_t *interface)
     iface = &interface->u.ap.iface;
     iface->interfaces = &radio->interfaces;
     iface->conf = &radio->iconf;
-    strcpy(iface->phy, radio->name);
+    /*CID 277488, 277385 Destination buffer too small*/
+    strncpy(iface->phy, radio->name, sizeof(iface->phy) - 1);
+    iface->phy[sizeof(iface->phy) - 1] = '\0';
     iface->state = HAPD_IFACE_ENABLED;
 
     iface->num_bss = 1;
@@ -1532,6 +1534,15 @@ int update_hostap_iface(wifi_interface_info_t *interface)
     if (iface->current_mode == NULL) {
         wifi_hal_error_print("%s:%d failed to get mode, interface: %s hw mode: %d, freq: %d\n",
             __func__, __LINE__, interface->name, iface->conf->hw_mode, iface->freq);
+        /*CID 338904, 338903 - Resource leak */
+        if (preassoc_supp_rates) {
+           os_free(preassoc_supp_rates);
+           preassoc_supp_rates = NULL;
+        }
+        if (preassoc_basic_rates) {
+           os_free(preassoc_basic_rates);
+           preassoc_basic_rates = NULL;
+        }
         return RETURN_ERR;
     }
 #else
