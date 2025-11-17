546cd8253b317877e58168a8c51be0d2474e87c8
Srijeyarankesh <58742948+Srijeyarankesh@users.noreply.github.com>
2025-11-17 16:10:20 +0530

    RDKB 62001: 6G STA Mode support for XBs (#452)
    * Cast security->mfp to enum mfp_options
    * Cast MGMT_FRAME_PROTECTION_REQUIRED to enum type

--- src/wifi_hal_nl80211.c
+++ src/wifi_hal_nl80211.c
@@ -9305,10 +9305,10 @@ int nl80211_connect_sta(wifi_interface_info_t *interface)
         security->mode == wifi_security_mode_wpa3_enterprise ||
         security->mode == wifi_security_mode_wpa3_transition) {
         // WPA3 REQUIRES MFP
-        wpa_conf.ieee80211w = MGMT_FRAME_PROTECTION_REQUIRED;
+        wpa_conf.ieee80211w = (enum mfp_options)MGMT_FRAME_PROTECTION_REQUIRED;
         wpa_conf.group_mgmt_cipher = WPA_CIPHER_AES_128_CMAC;
     } else {
-        wpa_conf.ieee80211w = security->mfp;
+        wpa_conf.ieee80211w = (enum mfp_options)security->mfp;
     }
 #endif
 
