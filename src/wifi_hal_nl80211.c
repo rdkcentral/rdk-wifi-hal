e2fea68a17f04ea75a52e1c1efa14c93578008b4
Sundram Patel <sundram.p@tataelxsi.co.in>
2026-01-08 16:59:35 +0530

    RDKBACCL-1405: MLD link interface transmit power
    -Added the nested netlink attribute for MLD Info.
    -Attribute NL80211_ATTR_MLO_LINKS.
    -Retrived the current tx power for interface linked with MLD.
    
    Signed-off-by: Sundram Patel <sundram.p@tataelxsi.co.in>

--- src/wifi_hal_nl80211.c
+++ src/wifi_hal_nl80211.c
@@ -18690,7 +18690,7 @@ static u8 *wifi_drv_get_mbssid_config(void *priv, u8 *eid)
 
 static int get_radio_txpwr_handler(struct nl_msg *msg, void *arg)
 {
-    unsigned int tx_pwr = 0;
+    int have_txpwr = 0;
     struct nlattr *tb[NL80211_ATTR_MAX + 1];
     struct genlmsghdr *gnlh = nlmsg_data(nlmsg_hdr(msg));
     unsigned long *tx_pwr_dbm = (unsigned long *)arg;
@@ -18701,13 +18701,42 @@ static int get_radio_txpwr_handler(struct nl_msg *msg, void *arg)
         return NL_SKIP;
     }
 
-    if (tb[NL80211_ATTR_WIPHY_TX_POWER_LEVEL] == NULL) {
-        wifi_hal_error_print("%s:%d Radio tx power attribute is missing\n", __func__, __LINE__);
-        return NL_SKIP;
+    if (tb[NL80211_ATTR_WIPHY_TX_POWER_LEVEL]) {
+        int txp_mbm = nla_get_s32(tb[NL80211_ATTR_WIPHY_TX_POWER_LEVEL]);
+	*tx_pwr_dbm = txp_mbm / 100; /* mBm to dBm */
+        have_txpwr = 1;
+    } else {
+        wifi_hal_error_print("%s:%d Radio tx power attribute is missing, Checking MLO link if present.\n", __func__, __LINE__);
     }
 
-    tx_pwr = nla_get_u32(tb[NL80211_ATTR_WIPHY_TX_POWER_LEVEL]);
-    *tx_pwr_dbm = tx_pwr / 100; /* mBm to dBm */
+#if HOSTAPD_VERSION >= 211 && defined(CONFIG_GENERIC_MLO)
+    if (tb[NL80211_ATTR_MLO_LINKS]) {
+        struct nlattr *link;
+        int rem;
+
+        nla_for_each_nested(link, tb[NL80211_ATTR_MLO_LINKS], rem) {
+            struct nlattr *ltb[NL80211_ATTR_MAX + 1];
+            if (nla_parse_nested(ltb, NL80211_ATTR_MAX, link, NULL) < 0)
+                continue;
+
+            if (ltb[NL80211_ATTR_WIPHY_TX_POWER_LEVEL]) {
+                int link_txp_mbm = nla_get_s32(ltb[NL80211_ATTR_WIPHY_TX_POWER_LEVEL]);
+                int link_id = ltb[NL80211_ATTR_MLO_LINK_ID] ? nla_get_u8(ltb[NL80211_ATTR_MLO_LINK_ID]) : -1;
+                have_txpwr = 1;
+                wifi_hal_error_print("%s:%d MLO link %d txpower %d dBm\n", __func__, __LINE__, link_id, link_txp_mbm / 100);
+                *tx_pwr_dbm = link_txp_mbm / 100;
+            } else {
+                wifi_hal_error_print("%s:%d MLO link missing tx power attribute\n", __func__, __LINE__);
+            }
+        }
+    }
+#endif // HOSTAPD_VERSION >= 211 && CONFIG_GENERIC_MLO
+
+    if (!have_txpwr) {
+        wifi_hal_error_print("%s:%d Unable to determine tx power (radio or MLO link). Returning without update.\n", __func__, __LINE__);
+        return NL_SKIP;
+    }
+    wifi_hal_info_print("%s:%d Radio tx power:%d \n", __func__, __LINE__, *tx_pwr_dbm);
     return NL_SKIP;
 }
 
