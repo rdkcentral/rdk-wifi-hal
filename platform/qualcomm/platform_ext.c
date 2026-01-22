2789220aa352f2964a28af478ead2a0c3fe4b2a8
MG-kildon <MARIAGNANAKILDON_S@comcast.com>
2026-01-22 14:30:55 +0530

    upating sta to mesh_sta_

--- platform/qualcomm/platform_ext.c
+++ platform/qualcomm/platform_ext.c
@@ -478,7 +478,7 @@ int platform_set_radio(wifi_radio_index_t index, wifi_radio_operationParam_t *op
     char mode[16];
 
     radio = get_radio_by_rdk_index(index);
-    interface = wifi_hal_get_vap_interface_by_type(radio, "sta");
+    interface = wifi_hal_get_vap_interface_by_type(radio, "mesh_sta_");
     get_interface_name_from_vap_index(interface->vap_info.vap_index, interface_name);
 
 
