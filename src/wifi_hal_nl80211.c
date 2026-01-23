942c00fccefac892ddcf356e18c42037d11dca82
siddharth.nair1 <siddharth.nair@telekom-digital.com>
2026-01-23 09:48:15 +0000

    RDKBWIFI-202: Implementation of Traffic Separation in unified-wifi-mesh
    Reason for change: Added traffic seperation TLV implementation
    Test Procedure: Verify build is successfull and check if traffic seperation is functional
    Risks: Medium
    Priority: P2

--- src/wifi_hal_nl80211.c
+++ src/wifi_hal_nl80211.c
@@ -39,6 +39,7 @@
 #include <linux/rtnetlink.h>
 #include <netpacket/packet.h>
 #include <netlink/route/link/bridge.h>
+#include <netlink/route/link/vlan.h>
 #include "wifi_hal.h"
 #include "wifi_hal_priv.h"
 #include "wpa_auth_i.h"
@@ -50,6 +51,7 @@
 #include "ap/hs20.h"
 #include <sys/wait.h>
 #include <linux/if_ether.h>
+#include <linux/if_bridge.h>
 #ifdef __GLIBC__
 #include <netinet/ether.h>
 #endif
@@ -8483,9 +8485,169 @@ int nl80211_create_interfaces(wifi_radio_info_t *radio, wifi_vap_info_map_t *map
             return -1;
         }
     }
-
     return 0;
 }
+
+/*  VLAN IMPL
+ * 1. Create wifi interface "wifi0" - nl80211_create_interface/nl80211_update_interface
+ * 2. Create VLAN link reference "wifi0.12"
+ * 3. Enslave VLAN interface to the bridge
+ * 4. configure hostapd with VLAN interface name
+ */
+/*
+ * Add a vlan interface with name 'vlan_if_name', VLAN ID 'vid' and
+ * tagged interface 'if_name'.
+ *
+ * returns -1 on error
+ * returns 1 if the interface already exists
+ * returns 0 otherwise
+*/
+int nl80211_add_vlan(const char *if_name, int vid, const char *vlan_if_name)
+{
+	int err, ret = -1;
+	struct nl_sock *handle = NULL;
+	struct rtnl_link *rlink = NULL;
+	int if_idx = 0;
+
+	wifi_hal_error_print(MSG_ERROR, "VLAN: vlan_add(if_name=%s, vid=%d, "
+		   "vlan_if_name=%s)\n", if_name, vid, vlan_if_name);
+
+    	if ((os_strlen(if_name) + 1) > IFNAMSIZ) {
+		wifi_hal_error_print(MSG_ERROR, "Interface name too long: '%s'",
+			   if_name);
+		return -1;
+	}
+
+	if ((os_strlen(vlan_if_name) + 1) > IFNAMSIZ) {
+		wifi_hal_error_print(MSG_ERROR, "VLAN Interface name too long: '%s'",
+			   vlan_if_name);
+		return -1;
+	}
+
+	handle = nl_socket_alloc();
+	if (!handle) {
+		wifi_hal_error_print(MSG_ERROR, "VLAN: failed to open netlink socket");
+		goto vlan_add_error;
+	}
+
+	err = nl_connect(handle, NETLINK_ROUTE);
+	if (err < 0) {
+		wifi_hal_error_print(MSG_ERROR, "VLAN: failed to connect to netlink: %s",
+			   nl_geterror(err));
+		goto vlan_add_error;
+	}
+
+	err = rtnl_link_get_kernel(handle, 0, if_name, &rlink);
+	if (err < 0) {
+		/* link does not exist */
+		wifi_hal_error_print(MSG_ERROR, "VLAN: interface %s does not exist",
+			   if_name);
+		goto vlan_add_error;
+	}
+	if_idx = rtnl_link_get_ifindex(rlink);
+	rtnl_link_put(rlink);
+	rlink = NULL;
+
+	err = rtnl_link_get_kernel(handle, 0, vlan_if_name, &rlink);
+	if (err >= 0) {
+		/* link does exist */
+		rtnl_link_put(rlink);
+		rlink = NULL;
+		wifi_hal_error_print(MSG_ERROR, "VLAN: interface %s already exists",
+			   vlan_if_name);
+		ret = 1;
+		goto vlan_add_error;
+	}
+
+	rlink = rtnl_link_alloc();
+	if (!rlink) {
+		wifi_hal_error_print(MSG_ERROR, "VLAN: failed to allocate new link");
+		goto vlan_add_error;
+	}
+
+	err = rtnl_link_set_type(rlink, "vlan");
+	if (err < 0) {
+		wifi_hal_error_print(MSG_ERROR, "VLAN: failed to set link type: %s",
+			   nl_geterror(err));
+		goto vlan_add_error;
+	}
+
+	rtnl_link_set_link(rlink, if_idx);
+	rtnl_link_set_name(rlink, vlan_if_name);
+
+	err = rtnl_link_vlan_set_id(rlink, vid);
+	if (err < 0) {
+		wifi_hal_error_print(MSG_ERROR, "VLAN: failed to set link vlan id: %s",
+			   nl_geterror(err));
+		goto vlan_add_error;
+	}
+
+	err = rtnl_link_add(handle, rlink, NLM_F_CREATE);
+	if (err < 0) {
+		wifi_hal_error_print(MSG_ERROR, "VLAN: failed to create link %s for "
+			   "vlan %d on %s (%d): %s",
+			   vlan_if_name, vid, if_name, if_idx,
+			   nl_geterror(err));
+		goto vlan_add_error;
+	}
+
+	ret = 0;
+
+vlan_add_error:
+	if (rlink)
+		rtnl_link_put(rlink);
+	if (handle)
+		nl_socket_free(handle);
+	return ret;
+}
+
+
+int nl80211_remove_vlan(const char *if_name)
+{
+	int err, ret = -1;
+	struct nl_sock *handle = NULL;
+	struct rtnl_link *rlink = NULL;
+
+	wifi_hal_error_print(MSG_ERROR, "VLAN: vlan_rem(if_name=%s)", if_name);
+
+	handle = nl_socket_alloc();
+	if (!handle) {
+		wifi_hal_error_print(MSG_ERROR, "VLAN: failed to open netlink socket");
+		goto vlan_rem_error;
+	}
+
+	err = nl_connect(handle, NETLINK_ROUTE);
+	if (err < 0) {
+		wifi_hal_error_print(MSG_ERROR, "VLAN: failed to connect to netlink: %s",
+			   nl_geterror(err));
+		goto vlan_rem_error;
+	}
+
+	err = rtnl_link_get_kernel(handle, 0, if_name, &rlink);
+	if (err < 0) {
+		/* link does not exist */
+		wifi_hal_error_print(MSG_ERROR, "VLAN: interface %s does not exists",
+			   if_name);
+		goto vlan_rem_error;
+	}
+
+	err = rtnl_link_delete(handle, rlink);
+	if (err < 0) {
+		wifi_hal_error_print(MSG_ERROR, "VLAN: failed to remove link %s: %s",
+			   if_name, nl_geterror(err));
+		goto vlan_rem_error;
+	}
+
+	ret = 0;
+
+vlan_rem_error:
+	if (rlink)
+		rtnl_link_put(rlink);
+	if (handle)
+		nl_socket_free(handle);
+	return ret;
+}
+
 
 static int scan_results_handler(struct nl_msg *msg, void *arg)
 {
