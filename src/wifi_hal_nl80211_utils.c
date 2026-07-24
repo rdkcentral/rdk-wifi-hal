e9341a8059dd8277647407e097ec6bc1ab194efb
ssiras826 <ssiras826@cable.comcast.com>
2026-07-24 18:11:27 +0530

    RDKBACCL-1975 : Observing build error for Kernel 6.12 builds
    Reason for change: Recent kernel6.12 builds are broken, since kernel6.12 using updated code of rdk-wifilibhostap
    Test Procedure: Test the wifi sanity
    Risks: Low
    Priority: P1
    
    Signed-off-by: ssiras826 <ssiras826@cable.comcast.com>

--- src/wifi_hal_nl80211_utils.c
+++ src/wifi_hal_nl80211_utils.c
@@ -6501,7 +6501,9 @@ static int alloc_mld(wifi_interface_info_t *interface)
     hapd->mld = mld;
     mld->refcount++;
     mld->num_links = 0;
+#if !defined(KERNEL_6_12)
     mld->next_link_id = 0;
+#endif
 
     g_wifi_hal.mld_array.mld_count++;
 
@@ -6766,8 +6768,16 @@ int setup_mlo_vap(wifi_interface_info_t *interface, wifi_vap_info_t *new_vap_con
         struct hostapd_data *link_bss = NULL;
         for (uint8_t i = 0; i < MAX_NUM_MLD_LINKS; i++) {
             link_id_free = true;
+#if !defined(KERNEL_6_12)
             hapd->mld_link_id = hapd->mld->next_link_id % MAX_NUM_MLD_LINKS;
             hapd->mld->next_link_id++;
+#endif
+#if defined(KERNEL_6_12)
+	         if ((hapd->mld->allocated_links & BIT(i)) == 0) {
+                                hapd->mld_link_id = i;
+                                hapd->mld->allocated_links |= BIT(i);
+                        }
+#endif
             for_each_mld_link(link_bss, hapd) {
                 if (link_bss == hapd) {
                     continue;
