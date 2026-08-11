cae5d41348b6293f09542066b8c1447bcbd5d075
umasankar098 <umasankaran1163@gmail.com>
2026-08-11 20:56:00 +0530

    SHARMAN-4192 : Eco mode is not not working in 6.3 SDK (#817)
    Reason for change: 6.3 sdk Eco mode
    
    Test Procedure: Enable and disable eco mode
    
    Risks: low
    
    Priority: P0
    
    Signed-off-by: usi096 <umasankar.srinivasan@sky.uk>
    
    SHARMAN-4192 : Eco mode is not not working in 6.3 SDK (#820)
    
    * SHARMAN-4192 : Eco mode is not not working in 6.3 SDK
    
    Reason for change: added command
    Test Procedure: Enable and disable eco mode
    Risks: low
    Priority: P0
    
    Signed-off-by: Ramakanth Taraka <ramakanth.taraka@sky.uk>

--- platform/broadcom/platform.c
+++ platform/broadcom/platform.c
@@ -2919,7 +2919,11 @@ int platform_get_radio_phytemperature(wifi_radio_index_t index,
 #define GPIO_UNEXPORT_PATH "/sys/class/gpio/unexport"
 #define GPIO_DIRECTION_PATH "/sys/class/gpio/gpio%d/direction"
 #define GPIO_VALUE_PATH "/sys/class/gpio/gpio%d/value"
+#ifdef DSPS_ENABLED
+#define ECOMODE_SCRIPT_FILE "/lib/rdk/wifi.sh"
+#else
 #define ECOMODE_SCRIPT_FILE "/etc/init/wifi.sh"
+#endif // DSPS_ENABLED
 #define GPIO_DIRECTION_OUT "out"
 #define BUFLEN_2 2
 
@@ -2944,6 +2948,7 @@ static int enable_echo_feature_and_power_control_configs(void)
         wifi_hal_dbg_print("%s:%d cmd [%s] unsuccessful \n", __func__, __LINE__, cmd);
     }
 
+#if !defined(DSPS_ENABLED)
     snprintf(cmd, sizeof(cmd), "%s dpden 1", ECOMODE_SCRIPT_FILE);
     rc = system(cmd);
     if (rc == 0) {
@@ -2951,6 +2956,7 @@ static int enable_echo_feature_and_power_control_configs(void)
     } else {
         wifi_hal_dbg_print("%s:%d cmd [%s] unsuccessful \n", __func__, __LINE__, cmd);
     }
+#endif
 
     return rc;
 }
@@ -2967,8 +2973,14 @@ static int check_dpd_feature_enabled(void)
     char cmd[BUFLEN_128] = {0};
     char buf[BUFLEN_2] = {0};
 
+#ifdef DSPS_ENABLED
+    snprintf(cmd, sizeof(cmd), "sh %s dsps mode 2>/dev/null | cut -d '|' -f1",
+             ECOMODE_SCRIPT_FILE);
+#else
     snprintf(cmd, sizeof(cmd), "%s dpden",
              ECOMODE_SCRIPT_FILE);
+#endif
+
     if ((fp = popen(cmd, "r")) != NULL)
     {
         if (fgets(buf, sizeof(buf), fp) != NULL)
@@ -3191,12 +3203,21 @@ int platform_set_ecomode_for_radio(const int wl_idx, const bool eco_pwr_down)
     int rc = 0;
 
     /* Put radio into eco mode (power down) */
+#ifdef DSPS_ENABLED
+    if (eco_pwr_down)
+        snprintf(cmd, sizeof(cmd), "sh %s dsps power_down wl%d",
+                 ECOMODE_SCRIPT_FILE, wl_idx);
+    else
+        snprintf(cmd, sizeof(cmd), "sh %s dsps power_up wl%d",
+                 ECOMODE_SCRIPT_FILE, wl_idx);
+#else
     if (eco_pwr_down)
         snprintf(cmd, sizeof(cmd), "sh %s edpddn wl%d",
                  ECOMODE_SCRIPT_FILE, wl_idx);
     else
         snprintf(cmd, sizeof(cmd), "sh %s edpdup wl%d",
                  ECOMODE_SCRIPT_FILE, wl_idx);
+#endif
 
     rc = system(cmd);
     if (rc == 0)
@@ -3206,9 +3227,10 @@ int platform_set_ecomode_for_radio(const int wl_idx, const bool eco_pwr_down)
     else
     {
         wifi_hal_error_print("%s:%d cmd [%s] unsuccessful \n", __func__, __LINE__, cmd);
+        return RETURN_ERR;
     }
 
-    return rc;
+    return RETURN_OK;
 }
 #endif // defined (ENABLED_EDPD)
 
