449375ac327998f0d591f0495575b4d291fa4da8
Jonathan Wu <195466430+jonathanwu-csv@users.noreply.github.com>
2026-09-23 20:31:56 -0700

    Fix compilation error in platform.c

--- platform/broadcom/platform.c
+++ platform/broadcom/platform.c
@@ -1322,7 +1322,7 @@ int platform_set_radio_pre_init(wifi_radio_index_t index, wifi_radio_operationPa
     }
 
 #if defined(CONFIG_IEEE80211BE)
-#if defined(SCXER10_PORT)
+#if defined(SCXER10_PORT) && (LINUX_VERSION_CODE < KERNEL_VERSION(5, 0, 0))
     platform_set_eht(index, (operationParam->variant & WIFI_80211_VARIANT_BE) ? true : false);
 #elif defined(XB10_PORT)
     int eht_enab = (operationParam->variant & WIFI_80211_VARIANT_BE) ? 1 : 0;
